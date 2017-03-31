45289b4f62d8de533c57f0401af77cbbb43feb66
Domenico <dtropeano@hotmail.it>
2017-03-31 18:15:17 +0200

    Added TouchIDRecognizer

--- Cykas/ViewController.swift
+++ Cykas/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AVFoundation
 import PennyPincher
+import LocalAuthentication
 class ViewController: UIViewController, UITextFieldDelegate,AVCaptureMetadataOutputObjectsDelegate{
 
      private let pennyPincherGestureRecognizer = PennyPincherGestureRecognizer()
@@ -177,6 +178,9 @@ class ViewController: UIViewController, UITextFieldDelegate,AVCaptureMetadataOut
         
         let similarityString = String(format: "%.2f", similarity)
         gestureLabel.text = "Template: \(template.id), Similarity: \(similarityString)"
+        if(Double(similarityString)!>10.0){
+            AuthenticateWithTouchID()
+        }
     }
     
 
@@ -184,6 +188,84 @@ class ViewController: UIViewController, UITextFieldDelegate,AVCaptureMetadataOut
     @IBAction func didTapClear(_ sender: Any) {
         gestureLabel.text = ""
         gestureView.clear()
+    }
+
+    func AuthenticateWithTouchID() {
+        let context = LAContext()
+        
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(
+            LAPolicy.deviceOwnerAuthenticationWithBiometrics,
+            error: &error) {
+            
+            // Device can use TouchID
+            context.evaluatePolicy(
+                LAPolicy.deviceOwnerAuthenticationWithBiometrics,
+                localizedReason: "Access requires authentication",
+                reply: {(success, error) in
+                    DispatchQueue.main.async {
+                        
+                        if error != nil {
+                            
+                            switch error!._code {
+                                
+                            case LAError.Code.systemCancel.rawValue:
+                                self.notifyUser("Session cancelled",
+                                                err: error?.localizedDescription)
+                                
+                            case LAError.Code.userCancel.rawValue:
+                                self.notifyUser("Please try again",
+                                                err: error?.localizedDescription)
+                                
+                            case LAError.Code.userFallback.rawValue:
+                                self.notifyUser("Authentication",
+                                                err: "Password option selected")
+                                // Custom code to obtain password here
+                                
+                            default:
+                                self.notifyUser("Authentication failed",
+                                                err: error?.localizedDescription)
+                            }
+                            
+                        } else {
+                            self.notifyUser("Authentication Successful",
+                                            err: "You now have full access")
+                        }
+                    }
+            })
+        } else {
+            // Device cannot use TouchID
+            switch error!.code{
+                
+            case LAError.Code.touchIDNotEnrolled.rawValue:
+                notifyUser("TouchID is not enrolled",
+                           err: error?.localizedDescription)
+                
+            case LAError.Code.passcodeNotSet.rawValue:
+                notifyUser("A passcode has not been set",
+                           err: error?.localizedDescription)
+                
+            default:
+                notifyUser("TouchID not available",
+                           err: error?.localizedDescription)
+                
+            }
+        }
+    }
+    
+    func notifyUser(_ msg: String, err: String?) {
+        let alert = UIAlertController(title: msg,
+                                      message: err,
+                                      preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "OK",
+                                         style: .cancel, handler: nil)
+        
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true,
+                     completion: nil)
     }
 
 
